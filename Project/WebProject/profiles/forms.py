54e406216220461282f33ddfc4731d12e11d14cf
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-09-13 00:56:21 +0300

    Create forms.py

---
+++ Project/WebProject/profiles/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from WebProject.profiles.models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['bio']
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class DeleteProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for (_, field) in self.fields.items():
+            field.widget.attrs['readonly'] = 'readonly'
+
+    class Meta:
+        model = Profile
+        fields = ()
